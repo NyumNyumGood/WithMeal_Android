8090fd158180469f48b6efa4681684a289fa1f26
jooyae <jooyae3970@naver.com>
2021-11-25 22:20:23 +0900

    [feat] Set Entrance Spinner

--- app/src/main/java/org/android/wantedhackathon/signup/SignupFragment.kt
+++ app/src/main/java/org/android/wantedhackathon/signup/SignupFragment.kt
@@ -1,18 +1,16 @@
 package org.android.wantedhackathon.signup
 
 
-import android.content.DialogInterface
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import org.android.wantedhackathon.R
 import org.android.wantedhackathon.databinding.FragmentSignupBinding
 import org.android.wantedhackathon.util.AutoClearedValue
-import org.android.wantedhackathon.util.DatePickerDialog
 
 @AndroidEntryPoint
 class SignupFragment : Fragment() {
@@ -29,17 +27,12 @@ class SignupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showSelectEntranceSpinner()
     }
-//
-//    @RequiresApi(Build.VERSION_CODES.N)
-//    private fun selectEntranceYear() {
-//        binding.textInputLayout.setOnClickListener {
-//            val datePicker = DatePickerDialog(object : DialogInterface {
-//                override fun applyDate(year: String) {
-//                    binding.edittextEntrance.setText("$year")
-//                }
-//            })
-//            datePicker.show(requireParentFragment().parentFragmentManager, "picker")
-//        }
-//    }
+
+    private fun showSelectEntranceSpinner(){
+        val entrance = resources.getStringArray(R.array.spinner_entrance_year)
+        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_dropdown_item , entrance)
+        binding.buttonDown.adapter = adapter
+    }
 }
