4a2818f1c1802be740d31bec7fec1c5e8894bf76
jooyae <jooyae3970@naver.com>
2021-11-25 23:18:22 +0900

    [feat] Select Entrance Year

--- app/src/main/java/org/android/wantedhackathon/signup/ui/SignupFragment.kt
+++ app/src/main/java/org/android/wantedhackathon/signup/ui/SignupFragment.kt
@@ -1,21 +1,23 @@
-package org.android.wantedhackathon.signup
+package org.android.wantedhackathon.signup.ui
 
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.android.wantedhackathon.R
 import org.android.wantedhackathon.databinding.FragmentSignupBinding
+import org.android.wantedhackathon.signup.data.EntranceSpinnerModel
 import org.android.wantedhackathon.util.AutoClearedValue
 
 @AndroidEntryPoint
 class SignupFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentSignupBinding>()
-
+    private val listOfYear = ArrayList<EntranceSpinnerModel>()
+    private lateinit var spinnerAdapterYear: SpinnerAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,11 +30,21 @@ class SignupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         showSelectEntranceSpinner()
+        popBackStack()
     }
 
     private fun showSelectEntranceSpinner(){
         val entrance = resources.getStringArray(R.array.spinner_entrance_year)
-        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_dropdown_item , entrance)
-        binding.buttonDown.adapter = adapter
+        for (i in entrance.indices){
+            val year = EntranceSpinnerModel(entrance[i])
+            listOfYear.add(year)
+        }
+        spinnerAdapterYear = SpinnerAdapter(requireActivity(), R.layout.item_entrance_spinner , listOfYear)
+        binding.spinnerEntranceYear.adapter = spinnerAdapterYear
     }
+
+    private fun popBackStack(){
+        binding.buttonReturn.setOnClickListener { findNavController().popBackStack() }
+    }
+
 }
