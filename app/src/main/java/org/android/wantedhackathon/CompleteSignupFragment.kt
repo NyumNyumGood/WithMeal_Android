d2522f4df7cc58e347ec3d01593dcdd73f848a30
jooyae <jooyae3970@naver.com>
2021-11-26 14:23:42 +0900

    [style] Set Signup Information View

--- app/src/main/java/org/android/wantedhackathon/CompleteSignupFragment.kt
+++ app/src/main/java/org/android/wantedhackathon/CompleteSignupFragment.kt
@@ -7,18 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import org.android.wantedhackathon.databinding.FragmentCollegeCertifyBinding
+import org.android.wantedhackathon.databinding.FragmentCompleteSignupBinding
 import org.android.wantedhackathon.util.AutoClearedValue
 
 @AndroidEntryPoint
-class CollegeCertifyFragment : Fragment() {
-    private var binding by AutoClearedValue<FragmentCollegeCertifyBinding>()
+class CompleteSignupFragment : Fragment(){
+    private var binding by AutoClearedValue<FragmentCompleteSignupBinding>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = FragmentCollegeCertifyBinding.inflate(inflater, container, false).let{
+    ): View?  = FragmentCompleteSignupBinding.inflate(inflater, container, false).let {
         binding = it
         it.root
     }
@@ -26,14 +26,9 @@ class CollegeCertifyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         popBackStack()
-        completeSignup()
     }
 
     private fun popBackStack(){
         binding.buttonReturn.setOnClickListener { findNavController().popBackStack() }
-    }
-
-    private fun completeSignup(){
-        binding.buttonComplete.setOnClickListener { findNavController().navigate(R.id.action_collegeCertifyFragment_to_completeSignupFragment) }
     }
 }
