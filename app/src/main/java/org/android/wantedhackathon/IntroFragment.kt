3d84b1ca12a15a80cd5edb7ae9e92747312a073c
jooyae <jooyae3970@naver.com>
2021-11-26 13:31:46 +0900

    [feat]Set Coroutine Scope

--- app/src/main/java/org/android/wantedhackathon/IntroFragment.kt
+++ app/src/main/java/org/android/wantedhackathon/IntroFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
 import org.android.wantedhackathon.databinding.FragmentIntroBinding
 import org.android.wantedhackathon.util.AutoClearedValue
+import kotlin.coroutines.CoroutineContext
 
 @AndroidEntryPoint
-class IntroFragment : Fragment() {
+class IntroFragment : Fragment(), CoroutineScope {
     private var binding by AutoClearedValue<FragmentIntroBinding>()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + Job()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,5 +30,14 @@ class IntroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
+        launchScope()
+    }
+    private fun launchScope() {
+        launch {
+            delay(2000)
+            withContext(Dispatchers.Main) {
+                findNavController().navigate(R.id.action_introFragment_to_loginFragment)
+            }
+        }
     }
 }
